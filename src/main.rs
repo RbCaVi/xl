5534d7a630adb1789f67a6837e2c752cd8faab40
RbCaVi <robert@robertvail.info>
2026-09-05 12:37:22 -0400

    only print the token list once

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,11 @@ proc swap (in out a 4, in out b 4) {
     op set b temp
 }"#;
     println!("{}", content);
-    for token in Lexer::new(content) {
-        match token.value {
-            TokenValue::LPAR => {println!("LPAR");}
-            TokenValue::RPAR => {println!("RPAR");}
-            TokenValue::LBR => {println!("LBR");}
-            TokenValue::RBR => {println!("RBR");}
-            TokenValue::COMMA => {println!("COMMA");}
-            TokenValue::AMP => {println!("AMP");}
-            TokenValue::PROC => {println!("PROC");}
-            TokenValue::FN => {println!("FN");}
-            TokenValue::LABEL => {println!("LABEL");}
-            TokenValue::OP => {println!("OP");}
-            TokenValue::VAR => {println!("VAR");}
-            TokenValue::IN => {println!("IN");}
-            TokenValue::OUT => {println!("OUT");}
-            TokenValue::NAME(name) => {println!("NAME {}", name);}
-            TokenValue::INT(i) => {println!("INT {}", i);}
-        }
-    }
-    let tree = match parse(Lexer::new(content).map(|x| {println!("{:?}", x); x})) {
+    let tree = match parse(Lexer::new(content).map(|x| {println!("{:?}", x.value); x})) {
         Err(_) => return,
         Ok(tree) => tree,
     };
-    println!("{:#?}", tree);
+    //println!("{:#?}", tree);
     let compiled = compile(&tree);
     println!("{:#?}", compiled);
 }
